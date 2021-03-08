e9944fbb60c028aec8c34d5ca4069d1d3c320f00
JokusPokus <jakob.schmitt@code.berlin>
2021-03-08 13:24:47 +0100

    feat: Add Article model

--- article/models.py
+++ article/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+# Let's set a default category (which we could call "Misc", for example)
+# that products can fall back to in the unlikely event that we delete a category.
+DEFAULT_CATEGORY = 1
+
+
+class Article(models.model):
+
+    created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(ArticleCategory, default=DEFAULT_CATEGORY, on_delete=models.SET_DEFAULT)
+
+    class Meta:
+        ordering = ['created']
