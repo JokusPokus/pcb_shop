e536c7ae9c7945f7cc279038a7dd487c414158da
JokusPokus <jakob.schmitt@code.berlin>
2021-03-08 11:52:16 +0100

    feat: Add User models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+# from article.models import Article
 
-# Create your models here.
+
+class User(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=254)
+    defaultShippingAddressID = models.IntegerField()
+    defaultBillingAddressID = models.IntegerField()
+
+    class Meta:
+        ordering = ['created']
+
+
+class BasketItem(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created']
