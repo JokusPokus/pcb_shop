cd152c562dc08f04f7b8ec0b96eb99b212fa5d95
JokusPokus <jakob.schmitt@code.berlin>
2021-03-08 13:49:47 +0100

    feat: Add basic Board model

--- article/models.py
+++ article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Let's set a default category (which we could call "Misc", for example)
@@ -15,3 +16,14 @@ class Article(models.model):
         ordering = ['created']
 
 
+class Board(models.model):
+
+    created = models.DateTimeField(auto_now_add=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name='boards', on_delete=models.CASCADE)
+
+    gerberFileName = models.CharField(max_length=100)
+    gerberHash = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['created']
