544c7fa8d39f1df4a1397e9243a4b8fa156d2d85
JokusPokus <jakob.schmitt@code.berlin>
2021-03-11 14:23:35 +0100

    struct: Enable different settings/environments

--- pcb_shop/settings/base.py
+++ pcb_shop/settings/base.py
@@ -15,7 +15,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -73,15 +73,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pcb_shop.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
