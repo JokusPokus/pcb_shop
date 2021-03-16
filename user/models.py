f7b9a292767106c6a503d1f6798829e057cd437d
JokusPokus <jakob.schmitt@code.berlin>
2021-03-16 11:49:30 +0100

    refactor: Fix imports in user models

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# from article.models import Article
+from article.models import Article
 from user.address_management import Address
 
 
