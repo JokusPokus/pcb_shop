a62516e0566058a71d46173dc49a1b24498066b5
Neumi <9141110+Neumi@users.noreply.github.com>
2021-03-16 11:27:28 +0100

    feat: Add Order and Payment Models #12 addded Article model to order model

--- order/models.py
+++ order/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-import article.models
-import user.models
+from article.models import Article
 from user.models import Address
 
 
