9f1b538fd15b2bfd336027d569666b4c4ba648b3
JokusPokus <jakob.schmitt@code.berlin>
2021-03-26 12:06:57 +0100

    feat: Add ArticleSerializer

--- article/serializers.py
+++ article/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
-from .models import Board
+from .models import Board, Article
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = (
+            'created',
+            'category'
+        )
 
 
 class BoardSerializer(serializers.ModelSerializer):
+    article = ArticleSerializer()
 
     class Meta:
         model = Board
         fields = (
             'owner',
             'created',
+            'category',
             'dimensionX',
             'dimensionY',
         )
