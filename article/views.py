b3cd08e01191e4560a5e4ff6fdd0f003eef4972d
JokusPokus <jakob.schmitt@code.berlin>
2021-03-26 14:42:47 +0100

    doc: Add docstrings to article app views

--- article/views.py
+++ article/views.py
@@ -7,6 +7,9 @@ from .serializers import BoardSerializer
 
 
 class BoardList(generics.ListCreateAPIView):
+    """Provides functionality to list all PCBs the calling user has
+    created (GET) or to create a new PCB (POST).
+    """
     serializer_class = BoardSerializer
 
     def get_queryset(self):
@@ -18,11 +21,15 @@ class BoardList(generics.ListCreateAPIView):
         return Board.objects.filter(owner=user)
 
     def perform_create(self, serializer):
+        """Assures that the board is saved with the PCB category
+        and the calling user as owner.
+        """
         category = ArticleCategory.objects.get(name="PCB")
         serializer.save(owner=self.request.user, category=category)
 
 
 class BoardDetails(generics.RetrieveAPIView):
+    """Returns details for a specific PCB (GET)."""
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
     permission_classes = [IsAdminUser | (IsAuthenticated & IsBoardOwner)]
