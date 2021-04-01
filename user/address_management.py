3a11653d20d4a80628c03c10d86ce5b738706073
JokusPokus <jakob.schmitt@code.berlin>
2021-04-01 11:59:46 +0200

    migrate: Make migrations for Address model in user app

--- user/address_management.py
+++ user/address_management.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class Address(models.Model):
     """Model for users' addresses."""
-    receiver_first_name = models.CharField(max_length=30)
-    receiver_last_name = models.CharField(max_length=40)
-    street = models.CharField(max_length=40)
+    receiver_first_name = models.CharField(max_length=30, default="Max")
+    receiver_last_name = models.CharField(max_length=40, default="Mustermann")
+    street = models.CharField(max_length=40, default="Musterstraße")
     additional_line = models.CharField(max_length=40, null=True, blank=True)
     house_number = models.CharField(max_length=8, default="9999")
     zip_code = models.CharField(max_length=5, default="99999")
