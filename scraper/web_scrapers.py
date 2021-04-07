387d96d7510c953cee7aa4592673dd65c7a91e98
JokusPokus <jakob.schmitt@code.berlin>
2021-04-07 12:59:14 +0200

    feat: Create ABC for Crawler classes

--- scraper/web_scrapers.py
+++ scraper/web_scrapers.py
@@ -1,2 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+from abc import ABC, abstractmethod
+
+
+class Crawler(ABC):
+    @abstractmethod
+    def __init__(self, url):
+        self.url = url
+
+    def _get_html(self):
+        """Returns the HTML file returned from a GET request
+        to the Crawler instance's URL as bytes.
+        """
+        try:
+            r = requests.get(self.url, timeout=3)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as http_err:
+            raise Error(http_err)
+        except requests.exceptions.RequestException as err:
+            raise Error("Connection denied:", err)
+
+        return r.content
+
+    @abstractmethod
+    def get_board_options(self):
+        pass
+
