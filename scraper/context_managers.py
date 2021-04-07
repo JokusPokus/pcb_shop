681651a48104dffd8f74e19089b4001ff01793d2
JokusPokus <jakob.schmitt@code.berlin>
2021-04-07 23:21:57 +0200

    refactor: Suppress extensive logging for webdriver libraries

--- scraper/context_managers.py
+++ scraper/context_managers.py
@@ -1,6 +1,12 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+
+
+os.environ['WDM_PRINT_FIRST_LINE'] = 'False'
+os.environ['WDM_LOG_LEVEL'] = '0'
 
 
 class WebDriver:
@@ -12,9 +18,13 @@ class WebDriver:
         self.browser = browser
         self.opts = Options()
         self.opts.headless = True
+        self.opts.add_argument('log-level=3')
 
         if self.browser == "Chrome":
-            self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=self.opts)
+            self.driver = webdriver.Chrome(
+                ChromeDriverManager().install(),
+                chrome_options=self.opts
+            )
 
     def __enter__(self):
         return self.driver
