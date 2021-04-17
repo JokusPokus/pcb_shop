ed62abbd063691b6c968c066b6333e44aa13b2b1
JokusPokus <jakob.schmitt@code.berlin>
2021-04-17 16:20:52 +0200

    doc: Include pytest-mock in requirements.txt

--- article/tests/board_option_tests.py
+++ article/tests/board_option_tests.py
@@ -1,1 +1,15 @@
 import pytest
+
+from article.board_options import BoardOptionValidator
+
+
+class TestPrivateMethods:
+    pass
+
+
+class TestValidationSuccess:
+    pass
+
+
+class TestValidationFailure:
+    pass
