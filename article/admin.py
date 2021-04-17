ee306aafe3153cedbdda12f8ab149dff237d7de2
JokusPokus <jakob.schmitt@code.berlin>
2021-04-17 12:54:09 +0200

    feat: Add existing models to Django admin interface

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from article.models import (
+    Article,
+    ArticleCategory,
+    Board,
+    ExternalShop,
+    ExternalBoardOptions,
+    OfferedBoardOptions
+)
+
+admin.site.register(Article)
+admin.site.register(ArticleCategory)
+admin.site.register(Board)
+admin.site.register(ExternalShop)
+admin.site.register(ExternalBoardOptions)
+admin.site.register(OfferedBoardOptions)
