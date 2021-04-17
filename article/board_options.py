97be58b2f563c2f7050232df80eec57a30b384e6
JokusPokus <jakob.schmitt@code.berlin>
2021-04-17 14:55:13 +0200

    feat: Add method to validate set of internal board options

--- article/board_options.py
+++ article/board_options.py
@@ -2,6 +2,11 @@ from typing import Optional
 from article.models import ExternalShop
 
 
+class BoardOptionError(Exception):
+    """Raised when internal board option does not confirm with externally available one."""
+    pass
+
+
 class BoardOptionValidator:
     """Validates a set of internally offered board options
     against a set of externally available options.
@@ -19,4 +24,12 @@ class BoardOptionValidator:
         return self.shop.externalboardoptions_set.first()
 
     def is_valid(self, options: dict):
-        pass
+        """Returns True if the complete set of internal board options
+        is validated against the set of externally available options.
+
+        Raises BoardOptionError otherwise.
+        """
+        for label, values in options.items():
+            if not self._validate_option(label, values):
+                raise BoardOptionError(f"The '{label}' option does not conform with externally available options.")
+            return True
