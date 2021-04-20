f915c48737169a7d50fa63960619c6fb0541d1e0
JokusPokus <jakob.schmitt@code.berlin>
2021-04-20 13:39:38 +0200

    feat: Implement board attribute validation in board serializer

--- article/serializers.py
+++ article/serializers.py
@@ -1,13 +1,20 @@
+from django.core.validators import ValidationError
 from rest_framework import serializers
+
 from .models import Board, Article, OfferedBoardOptions
+from .validators import AttributeValidator
 
 
 class BoardSerializer(serializers.ModelSerializer):
     owner = serializers.CharField(source="owner.email", read_only=True)
     category = serializers.CharField(source="category.name", read_only=True)
 
-    def validate_attributes(self):
-        pass
+    def validate_attributes(self) -> None:
+        validator = AttributeValidator()
+        try:
+            validator.validate(self.attributes)
+        except ValidationError as e:
+            raise serializers.ValidationError("Not all chosen attributes are currently available:", e)
 
     class Meta:
         model = Board
