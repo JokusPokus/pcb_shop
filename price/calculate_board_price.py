fa2529298000c076b002bb6aaf004bf611683274
JokusPokus <jakob.schmitt@code.berlin>
2021-04-22 11:52:06 +0200

    feat: Add toy price calculator

---
+++ price/calculate_board_price.py
@@ -0,0 +1,11 @@
+from article.models import Board
+from random import randint
+
+
+class BoardPriceCalculator:
+    """A class that calculates a price for a given board configuration."""
+    @staticmethod
+    def calculate_price(board: Board) -> float:
+        """Returns the current price for a given board configuration."""
+        toy_price = randint(200, 1000) / 100
+        return toy_price
