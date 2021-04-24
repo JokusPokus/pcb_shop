a4703bfcf5f6e087238b425614055a5cec6e6594
JokusPokus <jakob.schmitt@code.berlin>
2021-04-24 11:26:07 +0200

    refactor: Split up order items handler function

--- order/models.py
+++ order/models.py
@@ -67,20 +67,37 @@ class Article2Order(models.Model):
         unique_together = ['order', 'article']
 
 
+def get_board_attrs(basket_item: BasketItem) -> dict:
+    """Returns the board attributes of a given basket item."""
+    board = Board.objects.get(pk=basket_item.article.pk)
+    return board.attributes
+
+
+def create_order_item(basket_item: BasketItem, order: Order, calculator: BoardPriceCalculator) -> Article2Order:
+    """Creates an order item based on a given basket item.
+    The order item is returned, but not saved in the database yet.
+    """
+    board_attrs = get_board_attrs(basket_item)
+    unit_price = calculator.calculate_price(board_attrs)
+    order_item = Article2Order(
+        article=basket_item.article,
+        order=instance,
+        unit_price=unit_price,
+        quantity=1
+    )
+    return order_item
+
+
 @receiver(post_save, sender=Order)
 def handle_order_items(sender, instance, created, **kwargs):
+    """Takes care that upon Order creation, all the user's basket items are added
+    to the order and are then deleted from the user's basket.
+    """
     if created:
         basket_items = BasketItem.objects.filter(owner=instance.user)
         calculator = BoardPriceCalculator()
 
         for basket_item in basket_items:
-            board = Board.objects.get(pk=basket_item.article.pk)
-            unit_price = calculator.calculate_price(board.attributes)
-            order_item = Article2Order(
-                article=basket_item.article,
-                order=instance,
-                unit_price=unit_price,
-                quantity=1
-            )
+            order_item = create_order_item(basket_item, instance, calculator)
             order_item.save()
             basket_item.delete()
