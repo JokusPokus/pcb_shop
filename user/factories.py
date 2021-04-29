c3856950e54f5d63b8038986e74c7cd1b3a1bce6
JokusPokus <jakob.schmitt@code.berlin>
2021-04-29 17:36:39 +0200

    feat: Create User Factory

---
+++ user/factories.py
@@ -0,0 +1,17 @@
+import factory
+from .models import User, BasketItem
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    email = factory.Faker("email")
+    password = factory.PostGenerationMethodCall('set_password', 'pcb_password')
+    is_superuser = False
+    is_staff = False
+    is_active = True
+
+
+class BasketItemFactory(factory.django.DjangoModelFactory):
+    pass
