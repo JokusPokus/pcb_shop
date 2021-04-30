4479e48208fd8624d7223f4458e80c7fd245939e
JokusPokus <jakob.schmitt@code.berlin>
2021-04-30 15:20:03 +0200

    feat: Register Address model in Django Admin

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
 from user.models import BasketItem
+from user.address_management import Address
+
 
 admin.site.register(BasketItem)
+admin.site.register(Address)
