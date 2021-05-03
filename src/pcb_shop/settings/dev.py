8b47f3b8a2ba7c4ebe08b863dcaffbe5959359e7
JokusPokus <jakob.schmitt@code.berlin>
2021-05-03 10:40:36 +0200

    refactor: Clean up dev settings

--- src/pcb_shop/settings/dev.py
+++ src/pcb_shop/settings/dev.py
@@ -1,17 +1,6 @@
 from .base import *
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'pcb_shop_dev',
-#         'USER': 'web_app',
-#         'PASSWORD': '12093361',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
