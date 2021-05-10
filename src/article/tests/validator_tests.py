a84fa10db3390dfa47ed89cbbd6768ada009ffed
JokusPokus <jakob.schmitt@code.berlin>
2021-05-10 22:50:28 +0200

    test: Add tests for BoardOptionsValidator

--- src/article/tests/validator_tests.py
+++ src/article/tests/validator_tests.py
@@ -1,17 +1,88 @@
+import pytest
+
+from django.core.exceptions import ValidationError
+
 from article.validators import BoardOptionValidator
 
 
 class TestBoardOptionValidator:
-    def test_validates_offered_board_options_correctly(self):
+    EXTERNAL_OPTIONS = {
+        "differentDesigns": {
+            "choices": [1, 2, 3]
+        },
+        "dimensionX": {
+            "range": {
+                "min": 10,
+                "max": 100
+            }
+        }
+    }
+
+    @pytest.mark.parametrize("choices, range_min, range_max", [
+        ([1, 2, 3], 10, 100),
+        ([1, 2], 30, 80),
+        ([1, 2, 3], 50, 50)
+    ])
+    def test_valid_options_do_not_raise_exception(self, choices, range_min, range_max):
         """GIVEN a BoardOptionValidator instantiated with a set of
         external board options
 
         WHEN the validate method is called with a set of internally
-        offered board options
+        offered, valid board options
 
-        THEN the correct boolean is returned.
+        THEN no exception is thrown.
         """
-        pass
+        validator = BoardOptionValidator(self.EXTERNAL_OPTIONS)
+
+        offered_options = {
+            "differentDesigns": {
+                "choices": choices
+            },
+            "dimensionX": {
+                "range": {
+                    "min": range_min,
+                    "max": range_max
+                }
+            }
+        }
+
+        try:
+            validator.validate(offered_options)
+        except ValidationError:
+            pytest.fail("Valid internal options raised ValidationError")
+
+    @pytest.mark.parametrize("choices, range_min, range_max", [
+        pytest.param([], 10, 100, id="empty choice list"),
+        pytest.param([1, 2, 4], 10, 100, id="choice list contains unavailable value"),
+        pytest.param([1, 2, 3], 9, 100, id="range minimum too low"),
+        pytest.param([1, 2, 3], 10, 101, id="range maximum too high"),
+        pytest.param([1, 2, 3], 50, 40, id="minimum is larger than maximum")
+    ])
+    def test_invalid_option_values_raise_validation_error(self, choices, range_min, range_max):
+        """GIVEN a BoardOptionValidator instantiated with a set of
+        external board options
+
+        WHEN the validate method is called with a set of internally
+        offered, invalid board options
+
+        THEN a ValidationError is thrown.
+        """
+        validator = BoardOptionValidator(self.EXTERNAL_OPTIONS)
+
+        offered_options = {
+            "differentDesigns": {
+                "choices": choices
+            },
+            "dimensionX": {
+                "range": {
+                    "min": range_min,
+                    "max": range_max
+                }
+            }
+        }
+
+        with pytest.raises(ValidationError):
+            validator.validate(offered_options)
 
 
 class TestAttributeValidator:
