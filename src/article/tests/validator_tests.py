2f8139cdb9de3bff087f6fdc0e6114fdc0bfd7b0
JokusPokus <jakob.schmitt@code.berlin>
2021-05-10 19:40:35 +0200

    doc: Add docstring

--- src/article/tests/validator_tests.py
+++ src/article/tests/validator_tests.py
@@ -2,7 +2,16 @@ from article.validators import BoardOptionValidator
 
 
 class TestBoardOptionValidator:
-    pass
+    def test_validates_offered_board_options_correctly(self):
+        """GIVEN a BoardOptionValidator instantiated with a set of
+        external board options
+
+        WHEN the validate method is called with a set of internally
+        offered board options
+
+        THEN the correct boolean is returned.
+        """
+        pass
 
 
 class TestAttributeValidator:
